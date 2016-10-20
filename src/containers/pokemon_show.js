2a4532f298668053191f58fc4690f6f0caabf4ac
Greg Perlman <gperlman27@gmail.com>
2016-10-20 17:31:11 -0400

    add abilities component

--- src/containers/pokemon_show.js
+++ src/containers/pokemon_show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { showPokemon } from '../actions/index';
 import { Link } from 'react-router';
+import Abilities from '../components/abilities';
 
 class PokemonShow extends Component {
   componentWillMount() {
@@ -11,16 +12,30 @@ class PokemonShow extends Component {
   render() {
     const { pokemon } = this.props; 
 
+
     if (!pokemon){
       return <div>Loading...</div>
     }
 
+    const abilities = pokemon.abilities.map(ability => ability.ability);
     console.log(pokemon);
 
     return (
       <div>
-        <h2>{pokemon.name}</h2>
-        <Link to="/" className='btn btn-default'>Back to Index</Link>
+        <div className="well">
+          <img src={pokemon.sprites.front_default} />
+          <h2>{pokemon.name.capitalizeFirstLetter()}</h2>
+          <hr />
+          <h3>Abilities:</h3>
+          <Abilities data={abilities} />
+        </div>
+
+
+        <Link to="/">
+          <button className='btn btn-secondary'>
+            Back to Index
+          </button>
+        </Link>
       </div>
     )
   }
